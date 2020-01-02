4d81ce3a62c82d14dfba671798d162dbecc8fa48
Mykhailo Roman <m96.roman@gmail.com>
2020-01-02 20:54:22 +0200

    feat(codility): added solution for MaxProductOfThree problem

---
+++ Algorithms/Codility/Sorting/MaxProductOfThree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Codility.Sorting
+{
+    public class MaxProductOfThree
+    {
+        public int solution(int[] A)
+        {
+            var length = A.Length;
+            var sorted = A.OrderBy(it => it).ToArray();
+
+            var productOfAllPositive = sorted[length - 1] * sorted[length - 2] * sorted[length - 3];
+            var productOfPositiveAndTwoNegative = sorted[length - 1] * sorted[0] * sorted[1];
+
+            return Math.Max(productOfAllPositive, productOfPositiveAndTwoNegative);
+        }
+    }
+}
