8bfcf0561bbe424b434084f4abbee426e9437596
Mykhailo Roman <m96.roman@gmail.com>
2020-01-12 01:20:35 +0200

    feat(codility): added solution for Dominator problem

---
+++ Algorithms/Codility/Leader/Dominator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codility.Leader
+{
+    public class Dominator
+    {
+        public int solution(int[] A)
+        {
+            var size = 0;
+            var value = -1;
+
+            foreach (var el in A)
+            {
+                if (size == 0)
+                {
+                    value = el;
+                    ++size;
+                }
+                else if(value != el)
+                {
+                    --size;
+                }
+            }
+
+            var countOfLeader = A.Count(it => it == value);
+
+            if (countOfLeader > A.Length / 2)
+            {
+                return Array.IndexOf(A, value);
+            }
+
+            return -1;
+        }
+    }
+}
