c214e6455f15474ecac8f2277baa79f243a18dac
Mykhailo Roman <m96.roman@gmail.com>
2020-02-11 06:08:25 +0200

    feat(codility): added solution for MinPerimeterRectangle problem

---
+++ Algorithms/Codility/PrimeAndCompositeNumbers/MinPerimeterRectangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Codility.PrimeAndCompositeNumbers
+{
+    public class MinPerimeterRectangle
+    {
+        public int solution(int N)
+        {
+            for (var i = (int)Math.Sqrt(N); i >= 1; --i)
+            {
+                if (N % i == 0)
+                {
+                    return 2 * (i + N/i);
+                }
+            }
+
+            return 0;
+        }
+    }
+}
