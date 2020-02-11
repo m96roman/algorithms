29d39d7011351edee2526fb6bf0f60d83e231f62
Mykhailo Roman <m96.roman@gmail.com>
2020-02-11 06:37:40 +0200

    feat(codility): added solution for CountFactors problem

---
+++ Algorithms/Codility/PrimeAndCompositeNumbers/CountFactors.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Codility.PrimeAndCompositeNumbers
+{
+    public class CountFactors
+    {
+        public int solution(int N)
+        {
+            var factorsCount = 0;
+            var sqrt = Math.Sqrt(N);
+
+            for (var i = 1; i < sqrt; ++i)
+            {
+                if (N % i == 0)
+                {
+                    factorsCount += 2;
+                }
+            }
+
+            if ((int)sqrt * (int)sqrt == N)
+            {
+                factorsCount += 1;
+            }
+
+            return factorsCount;
+        }
+    }
+}
