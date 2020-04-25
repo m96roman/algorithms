8f8af3dd4129ec919adb7f630b1092ec762f92b4
Mykhailo Roman <m96.roman@gmail.com>
2020-04-25 22:21:52 +0300

    feat(algotester): added Algotester project with MaryAndCookies problem solution

---
+++ Algorithms/Algotester/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Algotester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new MaryAndCookies().Run();
+        }
+    }
+}
