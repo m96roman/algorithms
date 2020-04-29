9d210e7d9b987ad166027e306afef04e2253b3fd
Mykhailo Roman <m96.roman@gmail.com>
2020-04-30 01:07:53 +0300

    feat(codility): added solution for Distinct problem

---
+++ Algorithms/Codility/Sorting/Distinct.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Codility.Sorting
+{
+    public class Distinct
+    {
+        public int solution(int[] A)
+        {
+            var set = new HashSet<int>();
+            foreach (var el in A)
+            {
+                set.Add(el);
+            }
+
+            return set.Count;
+        }
+    }
+}
