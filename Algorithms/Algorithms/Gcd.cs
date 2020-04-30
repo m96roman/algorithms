91fbc2019037833b7f6f156696339fd7565f081b
Mykhailo Roman <m96.roman@gmail.com>
2020-04-30 17:49:37 +0300

    feat(algorithms): added GCD algorithm

---
+++ Algorithms/Algorithms/Gcd.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithms
+{
+    static class Gcd
+    {
+        public static int Calculate(int[] numbers)
+        {
+            if(!numbers.Any())
+            {
+                throw new ArgumentException("empty array");
+            }
+
+            return numbers.Aggregate(numbers[1], (gcd, item) => GcdOfTwo(gcd, item));
+        }
+
+        private static int GcdOfTwo(int a, int b)
+        {
+            if(a == 0)
+            {
+                return b;
+            }
+
+            return GcdOfTwo(b % a, a);
+        }
+    }
+}
