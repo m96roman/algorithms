94995fea3b03d6fc5d497c59f3e6a673cab14cc0
Mykhailo Roman <m96.roman@gmail.com>
2020-07-07 12:09:11 +0300

    feat(codility): added solution for AbsDistinct problem

---
+++ Algorithms/Codility/CaterpillarMethod/AbsDistinct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codility.CaterpillarMethod
+{
+    public class AbsDistinct
+    {
+        public int solution(int[] A)
+        {
+            var set = new HashSet<int>();
+            foreach (var el in A)
+            {
+                set.Add(el == int.MinValue ? el : Math.Abs(el));
+            }
+
+            return set.Count;
+        }
+    }
+}
