3195f44fe21af9e0e05b2f661e203c96fcafd7cd
Mykhailo Roman <m96.roman@gmail.com>
2020-07-07 21:26:12 +0300

    feat(codility): added another solution for AbsDistinct problem

--- Algorithms/Codility/CaterpillarMethod/AbsDistinct.cs
+++ Algorithms/Codility/CaterpillarMethod/AbsDistinct.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Codility.CaterpillarMethod
 {
     public class AbsDistinct
     {
-        public int solution(int[] A)
+        public int solutionWithSet(int[] A)
         {
             var set = new HashSet<int>();
             foreach (var el in A)
@@ -14,6 +15,41 @@ namespace Codility.CaterpillarMethod
             }
 
             return set.Count;
+        }
+
+        public int solution(int[] A)
+        {
+            var sequence = A
+                .SkipWhile(it => it == int.MinValue)
+                .ToArray();
+
+            var left = 0;
+            var right = sequence.Length - 1;
+
+            var absDistinctCount = A.Length - sequence.Length;
+            
+            while (left <= right)
+            {
+                while (left + 1 <= right && sequence[left] == sequence[left + 1])
+                {
+                    ++left;
+                }
+
+                while (right - 1 >= left && sequence[right] == sequence[right - 1])
+                {
+                    --right;
+                }
+
+                var lAbs = Math.Abs(sequence[left]);
+                var rAbs = Math.Abs(sequence[right]);
+
+                ++absDistinctCount;
+
+                left += lAbs >= rAbs ? 1 : 0;
+                right -= lAbs <= rAbs ? 1 : 0;
+            }
+
+            return absDistinctCount;
         }
     }
 }
