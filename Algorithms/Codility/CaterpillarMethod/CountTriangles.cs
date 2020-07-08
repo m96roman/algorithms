1a34b35d8423893738f224de78b997a354f1714d
Mykhailo Roman <m96.roman@gmail.com>
2020-07-09 02:42:06 +0300

    feat(codility): added solution for CountTriangles problem

---
+++ Algorithms/Codility/CaterpillarMethod/CountTriangles.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+
+namespace Codility.CaterpillarMethod
+{
+    public class CountTriangles
+    {
+        public int solution(int[] A)
+        {
+            if (A.Length < 3)
+            {
+                return 0;
+            }
+
+            var orderedSequence = A
+                .OrderBy(it => it)
+                .ToArray();
+
+            var countOfTriangles = 0;
+
+            for (var firstV = 0; firstV < orderedSequence.Length - 2; ++firstV)
+            {
+                var thirdV = firstV + 2;
+
+                for (var secondV = firstV + 1; secondV < orderedSequence.Length - 1; ++secondV)
+                {
+                    while (thirdV < orderedSequence.Length &&  orderedSequence[firstV] + orderedSequence[secondV] > orderedSequence[thirdV])
+                    {
+                        ++thirdV;
+                    }
+
+                    countOfTriangles += thirdV - secondV - 1;
+                }
+            }
+
+            return countOfTriangles;
+        }
+    }
+}
