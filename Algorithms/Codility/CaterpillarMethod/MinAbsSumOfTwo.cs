a3b788bc564d05aef9b23c4efeeb24174a68dde3
Mykhailo Roman <m96.roman@gmail.com>
2020-07-09 23:55:37 +0300

    feat(codility): added solution for MinAbsSumOfTwo problem

---
+++ Algorithms/Codility/CaterpillarMethod/MinAbsSumOfTwo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Codility.CaterpillarMethod
+{
+    public class MinAbsSumOfTwo
+    {
+        public int solution(int[] A)
+        {
+            var orderedSequence = A
+                .OrderBy(it => it)
+                .ToArray();
+
+            var minAbsSum = int.MaxValue;
+
+            var left = 0;
+            var right = orderedSequence.Length - 1;
+
+            while (left <= right)
+            {
+                var absSum = Math.Abs(orderedSequence[left] + orderedSequence[right]);
+                minAbsSum = Math.Min(minAbsSum, absSum);
+
+                if (Math.Abs(orderedSequence[left]) > Math.Abs(orderedSequence[right]))
+                {
+                    ++left;
+                }
+                else
+                {
+                    --right;
+                }
+            }
+
+            return minAbsSum;
+        }
+    }
+}
