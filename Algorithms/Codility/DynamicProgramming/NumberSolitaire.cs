27d9f3e84d4df36a93e3134d624b37521e5eb9f4
Mykhailo Roman <m96.roman@gmail.com>
2020-07-10 14:16:47 +0300

    feat(codilty): added solution for NumberSolitaire problem

---
+++ Algorithms/Codility/DynamicProgramming/NumberSolitaire.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codility.DynamicProgramming
+{
+    public class NumberSolitaire
+    {
+        public int solution(int[] A)
+        {
+            var results = Enumerable.Repeat(int.MinValue, 6).ToArray();
+            results[0] = A[0];
+            
+            for (var i = 1; i < A.Length; ++i)
+            {
+                results[i % 6] = results.Max() + A[i];
+            }
+
+            return results[(A.Length - 1) % 6];
+        }
+    }
+}
