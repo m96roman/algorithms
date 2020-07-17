987a1608cdebe200b8743739c80d63e9da1f12f9
Mykhailo Roman <m96.roman@gmail.com>
2020-07-18 01:56:14 +0300

    feat(codility): added solution for MaxDoubleSliceSum problem

---
+++ Algorithms/Codility.Tests/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System.IO.Compression;
+using Codility.DynamicProgramming;
+using Codility.MaximumSliceProblem;
+using Xunit;
+
+namespace Codility.Tests
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(new[] { 1, 5, 2, -2 }, 0)]
+        public void MinAbsSum(int[] input, int expected)
+        {
+            var algo = new MinAbsSum();
+            var actual = algo.solution(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(new[] { 3, 2, 6, -1, 4, 5, -1, 2 }, 17)]
+        [InlineData(new[] { 1, -4, 4, 8, 7 }, 12)]
+        [InlineData(new[] { 4, 5, 3 }, 0)]
+        [InlineData(new[] { -2, -4, -1, 1, -2 }, 1)]
+        [InlineData(new[] { 5, 17, 0, 3 }, 17)]
+        [InlineData(new[] { 5, 0, 17, 3 }, 17)]
+        [InlineData(new[] { 6, 1, 5, 6, 4, 2, 9, 4 }, 26)]
+        public void MaxDoubleSliceSum(int[] input, int expected)
+        {
+            var algo = new MaxDoubleSliceSum();
+            var actual = algo.solution(input);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Algorithms/Codility/MaximumSliceProblem/MaxDoubleSliceSum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Codility.MaximumSliceProblem
+{
+    public class MaxDoubleSliceSum
+    {
+        public int solution(int[] A)
+        {
+            var maxSumStartingAt = new int[A.Length];
+            var maxSumEndingAt = new int[A.Length];
+
+            for (var i = 1; i < A.Length - 1; ++i)
+            {
+                maxSumEndingAt[i] = Math.Max(maxSumEndingAt[i - 1] + A[i], 0);
+            }
+
+            for (var i = A.Length - 2; i > 0; --i)
+            {
+                maxSumStartingAt[i] = Math.Max(maxSumStartingAt[i + 1] + A[i], 0);
+            }
+
+            var maxDoubleSlice = 0;
+
+            for (var i = 1; i < A.Length - 1; ++i)
+            {
+                maxDoubleSlice = Math.Max(maxDoubleSlice, maxSumEndingAt[i - 1] + maxSumStartingAt[i + 1]);
+            }
+
+            return maxDoubleSlice;
+        }
+    }
+}
