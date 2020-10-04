31c5c5bdfbc1fefd463c6d7fdf5d1d616185efc9
Mykhailo Roman <m96.roman@gmail.com>
2020-10-04 22:43:54 +0300

    fix(algotester): used custom code for new lines

--- Algorithms/Algotester.Tests/UnitTest1.cs
+++ Algorithms/Algotester.Tests/UnitTest1.cs
@@ -8,20 +8,17 @@ namespace Algotester.Tests
     {
         public string ExecuteWithInput(Action<TextReader, TextWriter> algo, string input)
         {
-            using (var reader = new StringReader(input))
-            {
-                using (var writer = new StringWriter())
-                {
-                    algo(reader, writer);
-                    return writer.ToString();
-                }
-            }
+            using var reader = new StringReader(input.Replace("|nl", Environment.NewLine));
+            using var writer = new StringWriter();
+
+            algo(reader, writer);
+            return writer.ToString().Replace(Environment.NewLine, "|nl");
         }
 
         [Theory]
-        [InlineData("74", "4\r\n")]
-        [InlineData("500", "1\r\n")]
-        [InlineData("707", "4\r\n")]
+        [InlineData("74", "4|nl")]
+        [InlineData("500", "1|nl")]
+        [InlineData("707", "4|nl")]
         public void DeputyMoney(string input, string expected)
         {
             var actual = ExecuteWithInput(AlgotesterSolutions1.DeputyMoney.Run, input);
@@ -30,7 +27,7 @@ namespace Algotester.Tests
         }
 
         [Theory]
-        [InlineData("2\r\n10 50 100\r\n70 10 50", "150\r\n")]
+        [InlineData("2|nl10 50 100|nl70 10 50", "150|nl")]
         public void BorschPotatoSalad(string input, string expected)
         {
             var actual = ExecuteWithInput(AlgotesterSolutions1.BorschPotatoSalad.Run, input);
@@ -39,8 +36,8 @@ namespace Algotester.Tests
         }
 
         [Theory]
-        [InlineData("2\r\n4 7", "3\r\n")]
-        [InlineData("7\r\n 3 1 1 2 10 7 4", "9\r\n")]
+        [InlineData("2|nl4 7", "3|nl")]
+        [InlineData("7|nl 3 1 1 2 10 7 4", "9|nl")]
         public void ElephantsCoach(string input, string expected)
         {
             var actual = ExecuteWithInput(AlgotesterSolutions1.ElephantsCoach.Run, input);
@@ -49,7 +46,7 @@ namespace Algotester.Tests
         }
 
         [Theory]
-        [InlineData("7 4", "Dragon\r\n")]
+        [InlineData("7 4", "Dragon|nl")]
         public void InterestingGame(string input, string expected)
         {
             var actual = ExecuteWithInput(AlgotesterSolutions1.InterestingGame.Run, input);
@@ -58,7 +55,7 @@ namespace Algotester.Tests
         }
 
         [Theory]
-        [InlineData("4\r\n4 7 47 74", "128\r\n")]
+        [InlineData("4|nl4 7 47 74", "128|nl")]
         public void MaryAndCookies(string input, string expected)
         {
             var actual = ExecuteWithInput(AlgotesterSolutions1.MaryAndCookies.Run, input);
@@ -67,8 +64,8 @@ namespace Algotester.Tests
         }
 
         [Theory]
-        [InlineData("3 5 7", "YES\r\n")]
-        [InlineData("12 10 25", "NO\r\n")]
+        [InlineData("3 5 7", "YES|nl")]
+        [InlineData("12 10 25", "NO|nl")]
         public void Oranges(string input, string expected)
         {
             var actual = ExecuteWithInput(AlgotesterSolutions1.Oranges.Run, input);
@@ -77,7 +74,7 @@ namespace Algotester.Tests
         }
 
         [Theory]
-        [InlineData("4 7", "3.5\r\n")]
+        [InlineData("4 7", "3.5|nl")]
         public void PetrosCake(string input, string expected)
         {
             var actual = ExecuteWithInput(AlgotesterSolutions1.PetrosCake.Run, input);
@@ -86,7 +83,7 @@ namespace Algotester.Tests
         }
 
         [Theory]
-        [InlineData("7 4\r\n7 1 4 7 6 3 4", "3\r\n")]
+        [InlineData("7 4|nl7 1 4 7 6 3 4", "3|nl")]
         public void Teeth(string input, string expected)
         {
             var actual = ExecuteWithInput(AlgotesterSolutions1.Teeth.Run, input);
@@ -95,7 +92,7 @@ namespace Algotester.Tests
         }
 
         [Theory]
-        [InlineData("4 7\r\n4 3 2 7\r\n5 2 1 7 6 4 1", "3\r\n")]
+        [InlineData("4 7|nl4 3 2 7|nl5 2 1 7 6 4 1", "3|nl")]
         public void Helloween(string input, string expected)
         {
             var actual = ExecuteWithInput(AlgotesterSolutions1.Helloween.Run, input);
@@ -104,8 +101,8 @@ namespace Algotester.Tests
         }
 
         [Theory]
-        [InlineData("RURU\r\n2 0", "YES\r\n")]
-        [InlineData("RRRRRR\r\n1 1", "NO\r\n")]
+        [InlineData("RURU|nl2 0", "YES|nl")]
+        [InlineData("RRRRRR|nl1 1", "NO|nl")]
         public void Robot(string input, string expected)
         {
             var actual = ExecuteWithInput(AlgotesterSolutions1.Robot.Run, input);
@@ -114,7 +111,7 @@ namespace Algotester.Tests
         }
 
         [Theory]
-        [InlineData("4\r\n3108 1624 5600 2268", "450\r\n")]
+        [InlineData("4|nl3108 1624 5600 2268", "450|nl")]
         public void VerkhovnaRada(string input, string expected)
         {
             var actual = ExecuteWithInput(AlgotesterSolutions1.VerkhovnaRada.Run, input);
@@ -123,7 +120,7 @@ namespace Algotester.Tests
         }
 
         [Theory]
-        [InlineData("30\r\nVVKVKKVVVVKVKKKKKVVKKVKKKKVVVV", "1:0\r\n2:4\r\n")]
+        [InlineData("30|nlVVKVKKVVVVKVKKKKKVVKKVKKKKVVVV", "1:0|nl2:4|nl")]
         public void KoliaVasiaTennis(string input, string expected)
         {
             var actual = ExecuteWithInput(AlgotesterSolutions1.KoliaVasiaTennis.Run, input);
@@ -132,7 +129,7 @@ namespace Algotester.Tests
         }
 
         [Theory]
-        [InlineData("2 3\r\n2 1 1\r\n1 0 0", "5\r\n1 3 1\r\n1 2 1\r\n2 1 1\r\n1 1 2\r\n1 1 1\r\n")]
+        [InlineData("2 3|nl2 1 1|nl1 0 0", "5|nl1 3 1|nl1 2 1|nl2 1 1|nl1 1 2|nl1 1 1|nl")]
         public void PotatoInBasement(string input, string expected)
         {
             var actual = ExecuteWithInput(AlgotesterSolutions1.PotatoInBasement.Run, input);
@@ -141,7 +138,7 @@ namespace Algotester.Tests
         }
 
         [Theory]
-        [InlineData("5\r\n4 7 2 10 3", "3\r\n")]
+        [InlineData("5|nl4 7 2 10 3", "3|nl")]
         public void LongestIncreasingSubSequence(string input, string expected)
         {
             var actual = ExecuteWithInput(AlgotesterSolutions1.LongestIncreasingSubSequence.Run, input);
@@ -150,7 +147,7 @@ namespace Algotester.Tests
         }
 
         [Theory]
-        [InlineData("3 2", "31\r\n")]
+        [InlineData("3 2", "31|nl")]
         public void NewYearMath(string input, string expected)
         {
             var actual = ExecuteWithInput(AlgotesterSolutions1.NewYearMath.Run, input);
