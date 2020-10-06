8ac7d6784e10aab139cbc5d3e34d36ff3464778c
Mykhailo Roman <m96.roman@gmail.com>
2020-10-06 23:34:29 +0300

    feat(hackerrank): added solution for MaxArraySum problem

---
+++ Algorithms/HackerRank.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace HackerRank.Tests
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(new[] { -2, 1, 3, -4, 5 }, 8)]
+        [InlineData(new[] { 3, 7, 4, 6, 5 }, 13)]
+        [InlineData(new[] { 2, 1, 5, 8, 4 }, 11)]
+        [InlineData(new[] { 3, 5, -7, 8, 10 }, 15)]
+        [InlineData(new[] { 12 }, 12)]
+        public void MaxArraySum_MaxSubsetSum(int[] arr, int expected)
+        {
+            var algo = new MaxArraySum();
+            var actual = algo.MaxSubsetSum(arr);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Algorithms/HackerRank/MaxArraySum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRank
+{
+    public class MaxArraySum
+    {
+        public int MaxSubsetSum(int[] arr)
+        {
+            if (arr.Length == 1)
+            {
+                return arr[0];
+            }
+
+            var dp = arr.ToArray();
+
+            dp[1] = Math.Max(dp[0], dp[1]);
+
+            for (var i = 2; i < dp.Length; ++i)
+            {
+                dp[i] = Math.Max(Math.Max(dp[i], dp[i - 2] + dp[i]), dp[i - 1]);
+            }
+
+            return dp.Max();
+        }
+    }
+}
