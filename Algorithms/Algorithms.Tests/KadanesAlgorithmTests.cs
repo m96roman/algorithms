7cd94050b34ae585700a4317cb2e4030e0895d33
Mykhailo Roman <m96.roman@gmail.com>
2021-04-04 21:13:01 +0300

    feat(algorithms): added KadanesAlgorithm

---
+++ Algorithms/Algorithms.Tests/KadanesAlgorithmTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class KadanesAlgorithmTests
+    {
+        [Theory]
+        [InlineData(new[]{-2, -3, 4, -1, -2, 1, 5, -3}, 7)]
+        public void FindSubArrayMaxSum_ReturnsCorrectSum(int[] array, int expected)
+        {
+            var actual = KadanesAlgorithm.FindSubArrayMaxSum(array);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Algorithms/Algorithms/KadanesAlgorithm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public static class KadanesAlgorithm
+    {
+        public static int FindSubArrayMaxSum(int[] array)
+        {
+            var maxSum = int.MinValue;
+            var currentSum = 0;
+
+            foreach (var el in array)
+            {
+                currentSum = Math.Max(el, currentSum + el);
+                maxSum = Math.Max(maxSum, currentSum);
+            }
+
+            return maxSum;
+        }
+    }
+}
