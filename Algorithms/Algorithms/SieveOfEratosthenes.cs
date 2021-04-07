83c67e30df431c90103e318f602dc6c7609fd812
Mykhailo Roman <m96.roman@gmail.com>
2021-04-07 23:43:36 +0300

    feat(algorithm): added SieveOfEratosthenes algorithm

---
+++ Algorithms/Algorithms/SieveOfEratosthenes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms
+{
+    public static class SieveOfEratosthenes
+    {
+        public static int[] GetPrimesUpTo(int lastPrime)
+        {
+            var sieve = new bool[lastPrime + 1];
+
+            for (var i = 2; i * i < sieve.Length; ++i)
+            {
+                if (sieve[i * i])
+                {
+                    continue;
+                }
+
+                for (var j = i * i; j < sieve.Length; j += i)
+                {
+                    sieve[j] = true;
+                }
+            }
+
+            var primes = sieve
+                .Select((el, i) => new { value = i, isPrime = !el })
+                .Where(it => it.isPrime)
+                .Select(it => it.value).ToArray();
+
+            return primes;
+        }
+    }
+}
