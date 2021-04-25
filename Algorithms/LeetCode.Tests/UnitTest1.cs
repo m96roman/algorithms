22edf93580a63e1585b0ec65126233af9c18e558
Mykhailo Roman <m96.roman@gmail.com>
2021-04-25 23:41:45 +0300

    feat(leetcode): added solution for RotateImage problem

--- Algorithms/LeetCode.Tests/UnitTest1.cs
+++ Algorithms/LeetCode.Tests/UnitTest1.cs
@@ -522,5 +522,63 @@ namespace LeetCode.Tests
 
             return ReadNodes(0);
         }
+
+        [Theory]
+        [MemberData(nameof(RotateImage_Rotate_Data))]
+        public void RotateImage(int[][] matrix, int[][] expected)
+        {
+            var algo = new RotateImage();
+            algo.Rotate(matrix);
+
+            AssertMatrixEqual(matrix, expected);
+        }
+
+        public static IEnumerable<object[]> RotateImage_Rotate_Data()
+        {
+            yield return new object[]
+            {
+                new[]
+                {
+                    new []{1,2,3},
+                    new []{4,5,6},
+                    new []{7,8,9}
+                },
+                new[]
+                {
+                    new []{7,4,1},
+                    new []{8,5,2},
+                    new []{9,6,3}
+                }
+            };
+
+            yield return new object[]
+            {
+                new[]
+                {
+                    new []{5,1,9,11},
+                    new []{2,4,8,10},
+                    new []{13,3,6,7},
+                    new []{15,14,12,16}
+                },
+                new[]
+                {
+                    new []{15,13,2,5},
+                    new []{14,3,4,1},
+                    new []{12,6,8,9},
+                    new []{16,7,10,11}
+                }
+            };
+        }
+
+        private static void AssertMatrixEqual(int[][] matrix1, int[][] matrix2)
+        {
+            for (var i = 0; i < matrix1.Length; ++i)
+            {
+                for (var j = 0; j < matrix1.Length; ++j)
+                {
+                    Assert.Equal(matrix1[i][j], matrix2[i][j]);
+                }
+            }
+        }
     }
 }
---
+++ Algorithms/LeetCode/RotateImage.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode
+{
+    public class RotateImage
+    {
+        public void Rotate(int[][] matrix)
+        {
+            var n = matrix.Length;
+
+            for (var i = 0; i < n / 2; ++i)
+            {
+                for (var j = 1; j < n - i * 2; ++j)
+                {
+                    var remember = matrix[i + j][i];
+                    var last = n - i - 1;
+
+                    matrix[i + j][i] = matrix[last][i + j];
+                    matrix[last][i + j] = matrix[last - j][last];
+                    matrix[last - j][last] = matrix[i][last - j];
+                    matrix[i][last - j] = remember;
+                }
+            }
+        }
+    }
+}
